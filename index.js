b96074a43d12385f2d4c2ac2c08ec0d15453e18b
Liv <w.phrush@gmail.com>
2020-06-15 15:41:10 -0500

    incorporate github handler

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 require('dotenv').config();
-
 const express = require('express');
 var bodyParser = require('body-parser');
 const {
   PORT_SERVER,
 } = process.env;
 
+const {
+  GitHubRequestHandler
+} = require('./github.js');
+
 const app = express();
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
 app.use(bodyParser.json());
 
-app.get('/api/github/users', (req, res) => res.send('Hello World!'));
+app.get('/api/github/users', GitHubRequestHandler);
 
 app.listen(PORT_SERVER || 4000, () =>
   console.log(`API server listening at http://localhost:${PORT_SERVER || 4000}`),
