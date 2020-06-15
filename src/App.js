ba667b00e695c8a3821ee2a517ef334e73c43a25
Liv <w.phrush@gmail.com>
2020-06-15 17:54:26 -0500

    add cors to server

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
   const [searchValue, setSearchValue] = useState([]);
   const [users, setUsers] = useState([]);
-  const [userCursor, setUserCursor] = useState('');
+  // const [userCursor, setUserCursor] = useState('');
 
   function initialSearch() {
-    axios.get(`https://localhost:4000/api/github/user/${searchValue}`)
-      .then(console.log);
+    axios
+      .get(`http://localhost:4000/api/github/user/${searchValue}`)
+      .then(console.log)
+      .catch(console.error);
   }
 
   return (
     <div className="App bg-secondary container">
       <h1 className="h1">GitHub Users Search</h1>
-      <div class="input-group mb-3">
+      <div className="input-group mb-3">
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           placeholder="Username to search"
           onChange={(e) => setSearchValue(e.target.value)}
         />
-        <div class="input-group-append">
-          <button class="btn btn-primary" type="button" onClick={initialSearch}>
+        <div className="input-group-append">
+          <button className="btn btn-primary" type="button" onClick={initialSearch}>
             Search
           </button>
         </div>
