47e9824c40c75c71124ccd817c21bdd2cb26472a
Liv <w.phrush@gmail.com>
2020-06-15 17:49:30 -0500

    begin setting up front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [searchValue, setSearchValue] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [userCursor, setUserCursor] = useState('');
+
+  function initialSearch() {
+    axios.get(`https://localhost:4000/api/github/user/${searchValue}`)
+      .then(console.log);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-secondary container">
+      <h1 className="h1">GitHub Users Search</h1>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Username to search"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <div class="input-group-append">
+          <button class="btn btn-primary" type="button" onClick={initialSearch}>
+            Search
+          </button>
+        </div>
+      </div>
+      {users.map((user) => (
+        <div key={user.id}>{user.login}</div>
+      ))}
     </div>
   );
 }
