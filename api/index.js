5d1d528f0c12f4abfcef02b05fb8c1813d88382a
Liv <w.phrush@gmail.com>
2020-06-16 14:46:03 -0500

    move server files to api directory

---
+++ api/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+var cors = require('cors');
+const express = require('express');
+var bodyParser = require('body-parser');
+const {
+  PORT_SERVER,
+} = process.env;
+
+const {
+  GitHubRequestHandler,
+  GitHubRequestPaginationHandler,
+} = require('./github.js');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.get('/api/github/user', GitHubRequestHandler);
+app.get('/api/github/user/:username', GitHubRequestHandler);
+
+app.get('/api/github/user/:username/cursor/:cursor', GitHubRequestPaginationHandler);
+
+app.get('/api/github/user/:username/after/:after', GitHubRequestPaginationHandler);
+app.get('/api/github/user/:username/before/:before', GitHubRequestPaginationHandler);
+
+app.listen(PORT_SERVER || 4000, () =>
+  console.log(`API server listening at http://localhost:${PORT_SERVER || 4000}`),
+);
