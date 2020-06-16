19e36cd109bb818f31feb3a73beb2bd8411978c3
Liv <w.phrush@gmail.com>
2020-06-15 19:46:56 -0500

    add links to card, and user name

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,55 +1,67 @@
 import React from 'react';
 
 const UserCard = ({ info }) => {
-  console.log(info);
   if (!info.id) {
     return null;
   }
   return (
-  <div className="card mb-4">
-    <img src={info.avatarUrl} className="card-img-top" alt={info.login} />
-    <div className="card-body">
-      <h5 className="card-title">{info.login}</h5>
-      <p>
-        {info.bio}
-      </p>
+    <div className="card mb-4">
+      <img src={info.avatarUrl} className="card-img-top" alt={info.login} />
+      <div className="card-body">
+        <a href={info.url} target="_blank" rel="noopener noreferrer">
+          <h5 className="card-title">{info.login}</h5>
+        </a>
+        <p className="font-weight-bold">{info.name}</p>
+        <p>{info.bio}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          <a
+            href={`${info.url}?tab=repositories`}
+            className="d-flex justify-content-between"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Repositories:</span>
+            <span>{info.repositories.totalCount}</span>
+          </a>
+        </li>
+        <li class="list-group-item">
+          <a
+            href={`${info.url}?tab=followers`}
+            className="d-flex justify-content-between"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Followers:</span>
+            <span>{info.followers.totalCount}</span>
+          </a>
+        </li>
+        <li class="list-group-item">
+          <a
+            href={`${info.url}?tab=following`}
+            className="d-flex justify-content-between"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Following:</span>
+            <span>{info.following.totalCount}</span>
+          </a>
+        </li>
+        <li class="list-group-item">
+          <a
+            href={`${info.url}?tab=stars`}
+            className="d-flex justify-content-between"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Starred Repositories:</span>
+            <span>{info.starredRepositories.totalCount}</span>
+          </a>
+        </li>
+      </ul>
     </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item d-flex justify-content-between">
-        <span>
-          Repositories:
-        </span>
-        <span>
-          {info.repositories.totalCount}
-        </span>
-        </li>
-      <li class="list-group-item d-flex justify-content-between">
-        <span>
-          Followers:
-        </span>
-        <span>
-          {info.followers.totalCount}
-        </span>
-        </li>
-      <li class="list-group-item d-flex justify-content-between">
-        <span>
-          Following:
-        </span>
-        <span>
-          {info.following.totalCount}
-        </span>
-        </li>
-      <li class="list-group-item d-flex justify-content-between">
-        <span>
-          Starred Repositories:
-        </span>
-        <span>
-          {info.starredRepositories.totalCount}
-        </span>
-        </li>
-    </ul>
-  </div>
-);
+  );
 }
 export { UserCard };
 
